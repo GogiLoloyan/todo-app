09bf202a997b060632cf580224ac483d0c92f2cd
Gogi Loloyan <fiftyone157@gmail.com>
2025-12-11 20:09:57 +0400

    feat: Integrate todo fetching with useQuery hook

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,29 @@
-import { Text } from "@fluentui/react";
+import { Stack, Text } from "@fluentui/react";
+
+import { useQuery } from "./hooks/useQuery";
+import { todoService } from "./api/todoService";
 
 const App: React.FC = () => {
-  return <Text variant="xLarge">Todo App</Text>;
+  const { data } = useQuery({
+    queryKey: ["todos", "all"],
+    queryFn: (context) => todoService.getAll(context),
+    initialData: [],
+  });
+
+  return (
+    <Stack
+      tokens={{ padding: 20, childrenGap: 16 }}
+      style={{ maxWidth: 800, margin: "0 auto" }}
+    >
+      <Text variant="xLarge">Todo App</Text>
+
+      <ul>
+        {data?.map((todo) => (
+          <li key={todo.id}>{todo.name}</li>
+        ))}
+      </ul>
+    </Stack>
+  );
 };
 
 export default App;
